b18e9cb87ec41a03b83b9853c0d1e8976c7e8c16
Timmi187 <54154839+Timmi187@users.noreply.github.com>
2022-11-07 16:19:12 +0300

    Update conn.php

--- onlinechatting/onlinechatting/chat_system/conn.php
+++ onlinechatting/onlinechatting/chat_system/conn.php
@@ -1,7 +1,7 @@
 <?php
  
 //MySQLi Procedural
-$conn = mysqli_connect("localhost","root","","chat_system");
+$conn = mysqli_connect("sql107.epizy.com","epiz_32946508","yh4vdatB2T","epiz_32946508_fff");
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
